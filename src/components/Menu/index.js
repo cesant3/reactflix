ad85ccfdcaa5ae9be09ff7596b18d8cf8e9c36f7
cesant3 <cesantiago3@gmail.com>
2020-07-29 16:01:02 -0300

    Paginas de cadastro de categoria e video

--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -1,17 +1,18 @@
 import React from 'react';
-import Logo from '../../Assets/img/Logo.png'
-import './Menu.css'
+import { Link } from 'react-router-dom';
+import Logo from '../../Assets/img/Logo.png';
+import './Menu.css';
 //import ButtonLink from './components/ButtonLink';
 import Button from '../Button';
 
 function Menu() {
     return (
         <nav className="Menu">
-            <a href="/">
+            <Link to="/">
                 <img className='Logo' src={Logo} alt="Carlosflix"></img>
-            </a>
+            </Link>
         
-            <Button as="a" className="ButtonLink" href="/">
+            <Button as={Link} className="ButtonLink" to="/cadastro/video">
                 Novo Vídeo
             </Button>
         </nav>
